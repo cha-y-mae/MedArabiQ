4f457c7060801ef6d978397167116c59be47884c
Chaimae Abouzahir <107033012+cha-y-mae@users.noreply.github.com>
2025-06-25 13:02:40 +0400

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/models_config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# Required models for ArabicLLMBench
-REQUIRED_MODELS = [
-    "inceptionai/jais-13b-chat",
-    "meta-llama/Llama-3.1-8B-Instruct"
-    "claude-3-5-sonnet-20240620"
-    ""
-]
-
-HF_CACHE_DIR = "./cache/hub"
